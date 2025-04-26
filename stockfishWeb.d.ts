2dbd2e6a4b53b6d62dd21dc3ad718c057cd2da94
Jonathan Gamble <gamblej@gmail.com>
2025-04-25 23:22:02 -0500

    reflect package rename in declared module

--- stockfishWeb.d.ts
+++ stockfishWeb.d.ts
@@ -1,14 +1,14 @@
-declare module 'lila-stockfish-web' {
+declare module "@lichess-org/stockfish-web" {
   interface StockfishWeb {
     uci(command: string): void; // send uci command, receive async response via listen
-
-    listen: (data: string) => void; // attach listener here
 
     // index arguments are used for dual net sf builds, 0 for big, 1 for small, otherwise ignore
 
     setNnueBuffer(data: Uint8Array, index?: number): void; // load nnue as buffer
 
     getRecommendedNnue(index?: number): string; // returns a bare filename
+
+    listen: (data: string) => void; // attach listener here
 
     onError: (msg: string) => void; // attach error handler here
   }
